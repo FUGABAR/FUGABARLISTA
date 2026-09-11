3c81aa4dbb61809ad32f4f905721c7f95292c844
FUGABAR <marcelolimanery@gmail.com>
2026-09-11 15:46:47 -0300

    Export firebaseConfig for external use

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyD5fQNxwDcnorVZWn2kIVNZ0Ccj1_vgM7Y",
   authDomain: "fuga-lista.firebaseapp.com",
   projectId: "fuga-lista",
@@ -8,3 +7,4 @@ const firebaseConfig = {
   appId: "1:55589507858:web:afd282f1df0a3c45a799cb",
   measurementId: "G-1157S5TVQ5"
 };
+
